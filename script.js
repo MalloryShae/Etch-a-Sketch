9188b426530540af6b3f2574afcb5f24575074cc
Mallory Napier <mallorynapier@gmail.com>
2019-11-18 12:02:13 +0200

    Store user input for grid size in variables

---
+++ script.js
@@ -0,0 +1,7 @@
+let gridWidth = ""
+let gridHeight =""
+
+document.getElementById("createButton").addEventListener("click", function(){
+  gridWidth = document.getElementById("width").value;
+  gridHeight = document.getElementById("height").value;
+});
