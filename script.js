24dbc51d631eecf84516331e4ae04b8d98a84a7f
Mallory Napier <mallorynapier@gmail.com>
2019-12-06 12:01:31 +0200

    static 16x16 grid

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let gridWidth = ""
-let gridHeight =""
+let gridWidth = 16
+let gridHeight = 16
+const grid = document.querySelector(".grid")
 
-document.getElementById("createButton").addEventListener("click", function(){
-  gridWidth = document.getElementById("width").value;
-  gridHeight = document.getElementById("height").value;
-});
+// document.getElementById("createButton").addEventListener("click", function(){
+//   gridWidth = document.getElementById("width").value;
+//   gridHeight = document.getElementById("height").value;
+//   createGrid();
+//   fillGrid();
+// })
+
+const createGrid = function(){
+    grid.style.gridTemplateColumns = `repeat(${gridWidth}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridHeight}, 1fr)`;
+}
+
+const fillGrid = function(){
+  let totalSquares = (gridWidth * gridHeight);
+  for (let i=0 ; i < totalSquares; i++){
+    const gridItem = document.createElement('div');
+    gridItem.className = "grid-item"
+    grid.appendChild(gridItem);
+    console.log("grid item")
+  }
+}
+
+createGrid();
+fillGrid();
